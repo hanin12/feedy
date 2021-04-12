934dd188f2bcb0f45a90066aa20064d7e439f6a9
hanin12 <haninlibya118@gmail.com>
2021-04-12 09:24:37 +0200

    this is webview app

---
+++ app/src/main/java/com/example/feedy/SplashScreenActivity.kt
@@ -0,0 +1,75 @@
+package com.example.feedy
+
+import android.animation.ValueAnimator
+import android.content.Intent
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.Transformation
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+import java.util.*
+import kotlin.concurrent.schedule
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
+        startLoaderAnimate()
+        //4second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this@SplashScreenActivity, WebViewActivity::class.java))
+            //finish this activity
+
+            finish()
+            endLoaderAnimate()
+        },4000)
+
+    }
+
+
+
+    private fun endLoaderAnimate() {
+
+        loaderImage.visibility = View.VISIBLE
+        loaderImage.clearAnimation()
+    }
+
+
+    private fun startLoaderAnimate() {
+        val objectAnimator = object : Animation() {
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+                val startHeight = 170
+                val newHeight = (startHeight + (startHeight + 40) * interpolatedTime).toInt()
+                loaderImage.layoutParams.height = newHeight
+                loaderImage.requestLayout()
+            }
+
+            override fun initialize(width: Int, height: Int, parentWidth: Int, parentHeight: Int) {
+                super.initialize(width, height, parentWidth, parentHeight)
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+
+        objectAnimator.repeatCount = 1
+
+        objectAnimator.repeatMode = ValueAnimator.RESTART
+        objectAnimator.duration = 1000
+        loaderImage.startAnimation(objectAnimator)
+    }
+}
---
+++ app/src/main/java/com/example/feedy/WebViewActivity.kt
@@ -0,0 +1,213 @@
+package com.example.feedy
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.ConnectivityManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.provider.Browser
+import android.util.Log
+import android.view.KeyEvent
+import android.webkit.*
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+import kotlinx.android.synthetic.main.activity_web_view.*
+import java.net.URISyntaxException
+import java.util.*
+
+
+class WebViewActivity : AppCompatActivity() {
+    lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private val url = "https://feedy.ly/"
+    private var isAlreadyCreated = false
+    private var lastBackPressTime: Long = 0
+    private val mContext: Context? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+        swipeRefreshLayout = findViewById(R.id.swipe)
+
+        swipeRefreshLayout.setOnRefreshListener {
+
+            Handler().postDelayed(Runnable {
+                swipeRefreshLayout.isRefreshing = false
+                webView.reload()
+            }, 4000)
+        }
+
+        val connectionManager =
+                this@WebViewActivity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectionManager.activeNetworkInfo
+        if(networkInfo == null || !networkInfo.isConnected || !networkInfo.isAvailable){
+            webView.loadUrl("file:///android_asset/error.html")
+
+        }else{
+            webView.loadUrl(url)
+        }
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(false)
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowContentAccess = true
+        webView.settings.setAppCacheEnabled(true)
+        webView.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+         // life saver, do not remove
+
+        webView.webViewClient = object : WebViewClient() {
+
+            override fun onPageStarted(view: WebView?, url: String, favicon: Bitmap?) {
+
+
+
+                if (url.startsWith("tel:")) {
+                    webView.stopLoading()
+                    this@WebViewActivity.startActivity(
+                            Intent(
+                                    "android.intent.action.DIAL",
+                                    Uri.parse(url)
+
+                            )
+
+                    )
+
+                    true
+                } else if (!url.startsWith("mailto:")) {
+                    false
+
+                } else {
+
+                    try {
+webView.stopLoading()
+                        intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                        intent.scheme
+
+                        intent.putExtra(Browser.EXTRA_CREATE_NEW_TAB, true);
+                        intent.putExtra(Browser.EXTRA_APPLICATION_ID, mContext?.packageName);
+
+                        startActivity(intent)
+
+                    } catch (e: Exception) {
+                        e.printStackTrace();
+
+                    }
+
+                    true
+
+                }
+
+
+                super.onPageStarted(view, url, favicon)
+            }
+
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+
+
+
+            }
+
+            override fun onReceivedError(
+                    view: WebView?,
+                    request: WebResourceRequest?,
+                    error: WebResourceError?
+            ) {
+
+                webView.goBack()
+              super.onReceivedError(view, request, error)
+
+                val connectionManager =
+                        this@WebViewActivity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                val networkInfo = connectionManager.activeNetworkInfo
+                if(networkInfo == null || !networkInfo.isConnected || !networkInfo.isAvailable){
+                    webView.loadUrl("file:///android_asset/error.html")
+
+                }else{
+
+                }
+
+            }
+        }
+        webView.loadUrl(url)
+
+       }
+    override fun onPause() {
+        super.onPause()
+        webView.onPause()
+
+
+
+    }
+    override fun onBackPressed() {
+        if (this.webView.canGoBack()) {
+            val webView: WebView = this.webView
+            if (webView != null) {
+                webView.goBack()
+                Log.e("wv", "inside go back pressed")
+            }
+        } else if (lastBackPressTime < System.currentTimeMillis() - 4000) {
+
+            lastBackPressTime = System.currentTimeMillis()
+        } else {
+            Log.e("wv", "inside else sec")
+            super.onBackPressed()
+        }
+    }
+    override fun onResume() {
+
+        super.onResume()
+
+
+        this.webView.onResume();
+
+        if (isAlreadyCreated && !isNetworkAvailable()) {
+            isAlreadyCreated = false
+
+        }
+    }
+
+
+
+
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectionManager =
+            this@WebViewActivity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectionManager.activeNetworkInfo
+
+        return networkInfo != null && networkInfo.isConnectedOrConnecting
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+
+        return super.onKeyDown(keyCode, event)
+    }
+
+
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest): Boolean {
+
+        return true
+    }
+
+
+
+
+
+}
